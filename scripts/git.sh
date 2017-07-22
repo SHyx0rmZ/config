de1f098b992905b0c4d027f59b29bf062f6f58d4
Patrick Pokatilo <mail@shyxormz.net>
2017-07-22 04:40:14 +0200

    Use vim as editor for Git

--- scripts/git.sh
+++ scripts/git.sh
@@ -6,4 +6,5 @@ git_configure() {
   git_maybe_configure user.name 'Patrick Pokatilo'
   git_maybe_configure user.email 'mail@shyxormz.net'
   git_maybe_configure alias.graph 'log --all --decorate --graph'
+  git_maybe_configure core.editor 'vim'
 }
