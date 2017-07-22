64943a93d1f8465198f4d60a9e7270755a67e7cd
Patrick Pokatilo <mail@shyxormz.net>
2017-07-22 04:55:54 +0200

    Add helper_dontfail()

--- scripts.sh
+++ scripts.sh
@@ -1,3 +1,19 @@
+helper_dontfail() {
+  echo $- | grep e > /dev/null
+
+  local ERR_STATUS=$?
+
+  if [ ${ERR_STATUS} -eq 0 ]; then
+    set +e
+  fi
+
+  "$@"
+
+  if [ ${ERR_STATUS} -eq 0 ]; then
+    set -e
+  fi
+}
+
 source "${CONFIG_DIR}/scripts/apt.sh"
 source "${CONFIG_DIR}/scripts/git.sh"
 
