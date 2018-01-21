9709c8c5364dff2dc5acb88e30b90f9788188b4c
Patrick Pokatilo <mail@shyxormz.net>
2018-01-21 12:18:52 +0100

    Fix Elm setup

--- scripts/elm.sh
+++ scripts/elm.sh
@@ -1,7 +1,11 @@
-elm_setup() {
-  helper_dontfail which elm > /dev/null
+elm_package() {
+  command -v elm > /dev/null
 
   if [ $? -ne 0 ]; then
     helper_sudo npm install -g elm
   fi
+}
+
+elm_setup() {
+  helper_dontfail elm_package
 }
