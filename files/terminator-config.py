c67e904e6c63ece02380fc727fb03e0a77b8157a
Patrick Pokatilo <mail@shyxormz.net>
2018-10-18 21:21:36 +0200

    Create terminator config if it doesn't exist yet

--- files/terminator-config.py
+++ files/terminator-config.py
@@ -1,9 +1,18 @@
 from configobj import ConfigObj
-from os import environ
+from os import environ, makedirs
+from os.path import exists
 
-filename = environ['HOME'] + '/.config/terminator/config'
+dirname = environ['HOME'] + '/.config/terminator'
+filename = dirname + '/config'
+
+if not exists(dirname):
+    makedirs(dirname)
 
 config = ConfigObj(filename)
+if 'profiles' not in config:
+    config['profiles'] = {}
+if 'default' not in config['profiles']:
+    config['profiles']['default'] = {}
 config['profiles']['default']['font'] = 'M+ 2m Medium 13'
 config['profiles']['default']['use_system_font'] = 'False'
 config['profiles']['default']['scrollback_infinite'] = 'True'
