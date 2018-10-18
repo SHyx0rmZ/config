c23dbad9b62b64894ef2ef4e979084cc70cb4b85
Patrick Pokatilo <mail@shyxormz.net>
2018-10-18 22:49:37 +0200

    Global Git ignore file

--- scripts/git.sh
+++ scripts/git.sh
@@ -7,4 +7,9 @@ git_setup() {
   git_maybe_configure user.email 'mail@shyxormz.net'
   git_maybe_configure alias.graph 'log --all --decorate --graph'
   git_maybe_configure core.editor 'vim'
+  git_maybe_configure core.excludesfile "${HOME}/.gitignore_global"
+
+  if [ ! -f "${HOME}/.gitignore_global" ]; then
+    echo '/.idea/' > "${HOME}/.gitignore_global"
+  fi
 }
